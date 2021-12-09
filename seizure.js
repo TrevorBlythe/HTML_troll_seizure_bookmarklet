8604083e86c91a5bfd5586efcd272355550ed202
TrevorBlythe <77030926+TrevorBlythe@users.noreply.github.com>
2021-12-09 08:04:17 -0600

    added fortntie emote and fixed trivial mistake

--- seizure.js
+++ seizure.js
@@ -35,4 +35,29 @@ setInterval(function() {
 	}
 	myLayery.style.background = 'rgb(' + Math.random() * 255 + ',' + Math.random() * 255 + ',' + Math.random() * 255 + ')';
 	
- }
+  }
+    // Get the left, remove the "px" from the end and convert it to an integer.
+    var posX = parseInt(mover.style.left.replace(/px$/, '')) || 0;
+
+    // Add dir * dist
+    posX += dir * dist;
+
+    // If we are moving right and we've gone over the right edge...
+    if (dir == 1 && posX + mover.offsetWidth > parent.offsetWidth) {
+        // only move right to the edge...
+        posX -= posX + mover.offsetWidth - parent.offsetWidth;
+        // and change direction.
+        dir *= -1
+    // If we are moving left and we've gone over the left edge...
+    } else if (dir == -1 && posX < 0) {
+        // stop at zero...
+        posX = 0;
+        // and change direction...
+        dir *= -1;
+    }
+
+    // Set the new position
+    mover.style.left = posX + "px";
+}, 10);
+
+   
